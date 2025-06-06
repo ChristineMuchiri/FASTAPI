dc2097ca6da4973870b5cb2173b9022154736687
Christine-Muchiri <christinemuchiri749@gmail.com>
2025-06-06 12:41:43 +0300

    request body

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
 from enum import Enum
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+class Item(BaseModel):
+    name: str
+    description: str | None= None
+    price: float
+    tax: float | None = None
+    
 app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 @app.get("/")
 async def root():
